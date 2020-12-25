1b461c13f22b7bf3e1a617bae2ea06faa97e53fc
thomasmartin1783 <thomasmartin1783@gmail.com>
2020-12-25 14:41:25 -0600

    1. production build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Route, Switch, useLocation } from "react-router-dom";
+import { AnimatePresence } from 'framer-motion'
+
+
+
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
+import Base from './components/Base';
+import Toppings from './components/Toppings';
+import Order from './components/Order';
+import Modal from './components/Modal'
+
+
+
+
+
+// Animate Presence
+
+
+
 
 function App() {
+
+  // States
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+  const [showModal, setShowModal] = useState(false)
+
+
+  // Events
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base })
+  }
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter(item => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  }
+
+
+
+  const location = useLocation()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
+      <AnimatePresence exitBeforeEnter onExitComplete={() => { setShowModal(false) }}>
+        <Switch location={location} key={location.key}>
+          <Route path="/base">
+            <Base addBase={addBase} pizza={pizza} />
+          </Route>
+          <Route path="/toppings">
+            <Toppings addTopping={addTopping} pizza={pizza} />
+          </Route>
+          <Route path="/order">
+            <Order pizza={pizza} setShowModal={setShowModal} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </AnimatePresence>
+    </>
   );
 }
+
+
+
 
 export default App;
